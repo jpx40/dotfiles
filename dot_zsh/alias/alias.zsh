2b81a217cf0844d559d03e5a009dc6b5a5bf02e2
Jonas <92295347+jpx40@users.noreply.github.com>
2023-10-03 22:05:57 +0200

    upadate

--- dot_zsh/alias/alias.zsh
+++ dot_zsh/alias/alias.zsh
@@ -1,3 +1,15 @@
 alias hx=helix
 alias piu=python -m pip install -U
 alias golangci=golangci-lint
+
+alias vim=lvim
+alias vi=lvim
+
+
+alias cat=bat
+alias mk=mkdir
+
+alias steCom=bunx stencil generate
+alias sten=bunx stencil build --dev --watch --serve --cors
+
+alias ls=exa
