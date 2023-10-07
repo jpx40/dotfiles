2a8e6deeb6fd8656b03fa2e7afdd593402b5e366
Jonas <92295347+jpx40@users.noreply.github.com>
2023-10-07 23:35:07 +0200

    update

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -6,11 +6,72 @@
 
 -- lvim.format_on_save.enable = true
 
+-- function to display metals status in the statusline
+local function metals_status()
+  local status = vim.g["metals_status"]
+  if status == nil then
+    return ""
+  else
+    return status
+  end
+end
+
+-- As per the nvim-metals documentation, you should hook up metals_status into your
+-- status bar https://github.com/scalameta/nvim-metals/blob/main/doc/metals.txt#L101-L114
+local components = require("lvim.core.lualine.components")
+lvim.builtin.lualine.sections.lualine_c = {
+  -- NOTE: There is no way to append a component, so we need to include the components
+  -- here that are already supplied by lunarvim in `lualine_c`
+  components.diff,
+  components.python_env,
+  metals_status,
+}
 
 lvim.plugins = {
 
   "olexsmir/gopher.nvim",
   "leoluz/nvim-dap-go",
+
+
+  {
+    "scalameta/nvim-metals",
+    -- config = function()
+    --   require("user.metals").config()
+    -- end,
+  },
+  {
+    "peterhoeg/vim-qml",
+    event = "BufRead",
+    ft = { "qml" },
+  },
+  {
+    "peterhoeg/vim-qml",
+    event = "BufRead",
+    ft = { "qml" },
+  },
+  {
+    "windwp/nvim-spectre",
+    event = "BufRead",
+    config = function()
+      require("spectre").setup()
+    end,
+  },
+  -- lazy.nvim
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
 
 
 }
@@ -28,12 +89,15 @@ lvim.builtin.treesitter.ensure_installed = {
   "heex",
   "go",
   "gomod",
-  "cpp", "c",
+  "cpp",
+  "c",
   "rust",
   "zig",
   "python",
   "ocaml",
   "markdown",
+  "scala",
+  "markdown_inline",
 }
 
 lvim.lsp.installer.setup.ensure_installed = {
@@ -52,15 +116,19 @@ lvim.lsp.installer.setup.ensure_installed = {
 ------------------------
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { command = "goimports",   filetypes = { "go" } },
-  { command = "gofumpt",     filetypes = { "go" } },
-  --{ command = "ocp-indent",  filetypes = { "ml" } },
+  { command = "goimports",    filetypes = { "go" } },
+  { command = "gofumpt",      filetypes = { "go" } },
+  { command = "clang_format", filetypes = { "cpp", "c" } },
+
+  {
+    command = "prettier",
+    filetypes = { "html", "markdown", "css", "scss", "less", "graphql", "markdown_inline", "yaml",
+      "json", "sass", "svelte" }
+  },
+  { command = "qmlformat",   filetypes = { "qml" } },
+  { command = "deno_fmt",    filetypes = { "tsx", "ts", "js", "jsx", "json" } },
 
   { command = "ocamlformat", filetypes = { "ml" } }, }
-
--- lvim.format_on_save = {
---   pattern = { "*" },
--- }
 
 ------------------------
 -- Dap
@@ -178,6 +246,25 @@ table.insert(lvim.plugins, {
 
 })
 
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.scala", "*.sbt", "*.sc" },
+  callback = function()
+    local metals_config = require("metals").bare_config()
+    metals_config.on_attach = function(client, bufnr)
+      require("lvim.lsp").common_on_attach(client, bufnr)
+      require("metals").setup_dap()
+    end
+    metals_config.init_options.statusBarProvider = "on"
+    metals_config.settings = {
+      showImplicitArguments = false,
+      showInferredType = true,
+      excludedPackages = {},
+    }
+    require("metals").initialize_or_attach(metals_config)
+  end,
+  group = nvim_metals_group,
+})
 
 
 table.insert(lvim.plugins, {
@@ -238,10 +325,11 @@ table.insert(lvim.plugins, {
   lazy = false,
   config = function()
     require("nvim-ts-autotag").setup()
-  end
+  end,
 
 })
 
+lvim.builtin.dap.active = true
 
 lsp_manager.setup("html", {
   on_attach = on_attach,
@@ -317,6 +405,205 @@ lsp_manager.setup("emmet_language_server", {
 --   dependencies = { 'kkharji/sqlite.lua' }
 -- })
 
+
+require("noice").setup({
+  cmdline = {
+    enabled = true,   -- enables the Noice cmdline UI
+    view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+    opts = {},        -- global options for the cmdline. See section on views
+    ---@type table<string, CmdlineFormat>
+    format = {
+      -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+      -- view: (default is cmdline view)
+      -- opts: any options passed to the view
+      -- icon_hl_group: optional hl_group for the icon
+      -- title: set to anything or empty string to hide
+      cmdline = { pattern = "^:", icon = "", lang = "vim" },
+      search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+      search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+      filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+      lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+      help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+      input = {}, -- Used by input()
+      -- lua = false, -- to disable a format, set to `false`
+    },
+  },
+  messages = {
+    -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+    -- This is a current Neovim limitation.
+    enabled = true,              -- enables the Noice messages UI
+    view = "notify",             -- default view for messages
+    view_error = "notify",       -- view for errors
+    view_warn = "notify",        -- view for warnings
+    view_history = "messages",   -- view for :messages
+    view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+  },
+  popupmenu = {
+    enabled = true,  -- enables the Noice popupmenu UI
+    ---@type 'nui'|'cmp'
+    backend = "nui", -- backend to use to show regular cmdline completions
+    ---@type NoicePopupmenuItemKind|false
+    -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
+    kind_icons = {}, -- set to `false` to disable icons
+  },
+  -- default options for require('noice').redirect
+  -- see the section on Command Redirection
+  ---@type NoiceRouteConfig
+  redirect = {
+    view = "popup",
+    filter = { event = "msg_show" },
+  },
+  -- You can add any custom commands below that will be available with `:Noice command`
+  ---@type table<string, NoiceCommand>
+  commands = {
+    history = {
+      -- options for the message history that you get with `:Noice`
+      view = "split",
+      opts = { enter = true, format = "details" },
+      filter = {
+        any = {
+          { event = "notify" },
+          { error = true },
+          { warning = true },
+          { event = "msg_show", kind = { "" } },
+          { event = "lsp",      kind = "message" },
+        },
+      },
+    },
+    -- :Noice last
+    last = {
+      view = "popup",
+      opts = { enter = true, format = "details" },
+      filter = {
+        any = {
+          { event = "notify" },
+          { error = true },
+          { warning = true },
+          { event = "msg_show", kind = { "" } },
+          { event = "lsp",      kind = "message" },
+        },
+      },
+      filter_opts = { count = 1 },
+    },
+    -- :Noice errors
+    errors = {
+      -- options for the message history that you get with `:Noice`
+      view = "popup",
+      opts = { enter = true, format = "details" },
+      filter = { error = true },
+      filter_opts = { reverse = true },
+    },
+  },
+  notify = {
+    -- Noice can be used as `vim.notify` so you can route any notification like other messages
+    -- Notification messages have their level and other properties set.
+    -- event is always "notify" and kind can be any log level as a string
+    -- The default routes will forward notifications to nvim-notify
+    -- Benefit of using Noice for this is the routing and consistent history view
+    enabled = true,
+    view = "notify",
+  },
+  lsp = {
+    progress = {
+      enabled = true,
+      -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+      -- See the section on formatting for more details on how to customize.
+      --- @type NoiceFormat|string
+      format = "lsp_progress",
+      --- @type NoiceFormat|string
+      format_done = "lsp_progress_done",
+      throttle = 1000 / 30, -- frequency to update lsp progress message
+      view = "mini",
+    },
+    override = {
+      -- override the default lsp markdown formatter with Noice
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+      -- override the lsp markdown formatter with Noice
+      ["vim.lsp.util.stylize_markdown"] = false,
+      -- override cmp documentation with Noice (needs the other options to work)
+      ["cmp.entry.get_documentation"] = false,
+    },
+    hover = {
+      enabled = true,
+      silent = false, -- set to true to not show a message if hover is not available
+      view = nil,     -- when nil, use defaults from documentation
+      ---@type NoiceViewOptions
+      opts = {},      -- merged with defaults from documentation
+    },
+    signature = {
+      enabled = true,
+      auto_open = {
+        enabled = true,
+        trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+        luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+        throttle = 50,  -- Debounce lsp signature help request by 50ms
+      },
+      view = nil,       -- when nil, use defaults from documentation
+      ---@type NoiceViewOptions
+      opts = {},        -- merged with defaults from documentation
+    },
+    message = {
+      -- Messages shown by lsp servers
+      enabled = true,
+      view = "notify",
+      opts = {},
+    },
+    -- defaults for hover and signature help
+    documentation = {
+      view = "hover",
+      ---@type NoiceViewOptions
+      opts = {
+        lang = "markdown",
+        replace = true,
+        render = "plain",
+        format = { "{message}" },
+        win_options = { concealcursor = "n", conceallevel = 3 },
+      },
+    },
+  },
+  markdown = {
+    hover = {
+      ["|(%S-)|"] = vim.cmd.help,                       -- vim help links
+      ["%[.-%]%((%S-)%)"] = require("noice.util").open, -- markdown links
+    },
+    highlights = {
+      ["|%S-|"] = "@text.reference",
+      ["@%S+"] = "@parameter",
+      ["^%s*(Parameters:)"] = "@text.title",
+      ["^%s*(Return:)"] = "@text.title",
+      ["^%s*(See also:)"] = "@text.title",
+      ["{%S-}"] = "@parameter",
+    },
+  },
+  health = {
+    checker = true, -- Disable if you don't want health checks to run
+  },
+  smart_move = {
+    -- noice tries to move out of the way of existing floating windows.
+    enabled = true, -- you can disable this behaviour here
+    -- add any filetypes here, that shouldn't trigger smart move.
+    excluded_filetypes = { "cmp_menu", "cmp_docs", "notify" },
+  },
+  ---@type NoicePresets
+  presets = {
+    -- you can enable a preset by setting it to true, or a table that will override the preset config
+    -- you can also add custom presets that you can enable/disable with enabled=true
+    bottom_search = true,          -- use a classic bottom cmdline for search
+    command_palette = true,        -- position the cmdline and popupmenu together
+    long_message_to_split = false, -- long messages will be sent to a split
+    inc_rename = false,            -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,        -- add a border to hover docs and signature help
+  },
+  throttle = 1000 / 30,            -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
+  ---@type NoiceConfigViews
+  views = {}, ---@see section on views
+  ---@type NoiceRouteConfig[]
+  routes = {}, --- @see section on routes
+  ---@type table<string, NoiceFilter>
+  status = {}, --- @see section on statusline components
+  ---@type NoiceFormatOptions
+  format = {}, --- @see section on formatting
+})
 
 
 lvim.keys.normal_mode["e"] = "<Up>"
