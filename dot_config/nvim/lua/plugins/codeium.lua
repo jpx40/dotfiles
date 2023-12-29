da6b68344c2ebec955470a3d01ba9ad2571ad7df
Jonas <92295347+jpx40@users.noreply.github.com>
2023-12-29 13:41:57 +0100

    update

--- dot_config/nvim/lua/plugins/codeium.lua
+++ dot_config/nvim/lua/plugins/codeium.lua
@@ -1,18 +1,28 @@
-return {{
-    'Exafunction/codeium.vim',
-   config = function()
-          require("codeium").setup({
-      
-      -- Change '<C-g>' here to any keycode you like.
-        -- rewrite later    
-        
-      vim.keymap.set('i', '<C-h>', function() return vim.fn['codeium#Accept']() end, { expr = true }),
-  
-         --{'i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true }}
-      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true }),
-       vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true }),
-      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true }),
-        })
+return {
+  {
+    "Exafunction/codeium.nvim",
+    config = function()
+      require("codeium").setup({
+
+        -- Change '<C-g>' here to any keycode you like.
+        -- rewrite later
+
+        vim.keymap.set("i", "<C-h>", function()
+          return vim.fn["codeium#Accept"]()
+        end, { expr = true }),
+
+        --{'i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true }}
+        vim.keymap.set("i", "<c-;>", function()
+          return vim.fn["codeium#CycleCompletions"](1)
+        end, { expr = true }),
+        vim.keymap.set("i", "<c-,>", function()
+          return vim.fn["codeium#CycleCompletions"](-1)
+        end, { expr = true }),
+        vim.keymap.set("i", "<c-x>", function()
+          return vim.fn["codeium#Clear"]()
+        end, { expr = true }),
+      })
     end,
-  
-    },}
+  },
+}
+
--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,16 +1,11 @@
-return {-- new filetypes for prettier
-{"stevearc/conform.nvim",
-opts =  function(_, opts)
-  opts.list_extend(opts.formatters_by_ft, {
-        ["svelte"] = {"prettier"}, -- "--parser", "svelte"},
-   ["templ"] = {"prettier"},
-    ["html"] = {"prettier"},
-    ["css"] = {"prettier"},
-    ["gotmpl"] = {"prettier"},
-    ["heex"] = {"prettier"},
-    ["json"] = {"prettier"},
-  ["reason"] = {"refmt"},
+return { -- new filetypes for prettier
+  {
+    "stevearc/conform.nvim",
 
-  })
-end,
-},}
+    opts = {
+      formatters_by_ft = {
+        templ = { "prettier" },
+      },
+    },
+  },
+}
